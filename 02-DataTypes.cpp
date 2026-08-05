bccd7f4a9f11ce7396d2aaa531ebd6f1342cb65d
Ammar Malik <work.malikammar@gmail.com>
2026-08-05 05:53:44 +0500

    Leart Data Types

---
+++ 02-DataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Baymax";
+    int age = 20;
+    float height = 1.94;
+    double weight = 99.5;
+    char gender = 'M';
+    bool isStudent = true;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Weight: " << weight << endl;
+    cout << "Gender: " << gender << endl;
+    return 0;
+}
+
