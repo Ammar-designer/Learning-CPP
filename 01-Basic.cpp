b74e6843538bb74e5afc4d5498cfbc307cb39283
Ammar Malik <work.malikammar@gmail.com>
2026-08-05 05:53:17 +0500

    Leart Basics

--- 01-Basic.cpp
+++ 01-Basic.cpp
@@ -4,13 +4,7 @@
 using namespace std;
 
 int main() {
-    string name;
-    string lastName;
-    double M = 6E7;
-    cout << M << endl;
-    bool isTrue = true;
-    bool isFalse = false;
-    cout << isTrue << " " << isFalse << endl;
+    string name, lastName;
     cout << "What is your name? ";
     cin >> name ; 
     cout << "What is your last name? ";
