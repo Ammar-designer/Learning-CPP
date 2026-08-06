fca13b6814def275d4ccb7ccf13d38edf77534a5
Ammar Malik <work.malikammar@gmail.com>
2026-08-07 00:24:49 +0500

    Going Through Chapter 2 on learncpp.com

--- Learncpp.com/02-FunctionsAndFiles.cpp
+++ Learncpp.com/02-FunctionsAndFiles.cpp
@@ -2,10 +2,28 @@
 
 using namespace std;
 
-void printHello() {
-    cout << "Hello World!" << '\n';
+//Value Returning Function
+int baz() {
+    return 5;
+}
+
+//Non-Value Returning Function
+void foo() {
+    cout << "In foo()" << '\n';
+}
+
+//Function Inside a Function
+void moo() {
+    cout << "Start Of moo()" << '\n';   
+    foo();                              //Invokes Function foo()
+    cout << "End Of moo()" << '\n';
 }
 
 int main() {
-    printHello();   
+    cout << "Start Of main()" << '\n';
+    moo();                              //Invokes The Function moo()
+    cout << "End Of main()" << '\n';
+    //Prints The Value Returned By The Function baz()
+    cout << "Value Returned By baz() Is: " << baz() << '\n';
+    return EXIT_FAILURE;
 }
