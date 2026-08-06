7b4bfd29c7699afd6ee6f0f75accfd70b859fddd
Ammar Malik <work.malikammar@gmail.com>
2026-08-06 19:14:54 +0500

    Completed Chapter 1 of learncpp.com And Made My First Working C++ Program

--- Learncpp.com/01-FirstProgram.cpp
+++ Learncpp.com/01-FirstProgram.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << '\n';
+int main()
+{
+    cout << "Enter An Integer: " << '\n';
+
+    float num{};
+    cin >> num;
+
+    cout << "Double of " << num << " is: " << num * 2 << '\n';
+    cout << "Triple of " << num << " is: " << num * 3 << '\n';
+    cout << "Half of " << num << " is: " << num / 2 << '\n';
+
+    return 0;
 }
